7c3d0e99792b31962dc636f92e24b9344fc21e34
da_tian <da_tian@ssic.cn>
2019-01-28 14:57:14 +0800

    添加柯里化函数

--- index.js
+++ index.js
@@ -6,16 +6,33 @@
     }
     return res
   }
-  function getUrlParams(){
-    var url = location.search; 
-    var params = new Object();  
-    if (url.indexOf("?") != -1) {  
-       var str = url.substr(1);  
-       strs = str.split("&");  
-       for(var i = 0; i < strs.length; i ++) {  
-        params[strs[i].split("=")[0]]=unescape(strs[i].split("=")[1]);  
-       }  
-    }  
-    return params;  
+
+  function getUrlParams() {
+    var url = location.search;
+    var params = new Object();
+    if (url.indexOf("?") != -1) {
+      var str = url.substr(1);
+      strs = str.split("&");
+      for (var i = 0; i < strs.length; i++) {
+        params[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
+      }
+    }
+    return params
+  }
+  // 柯里化数字处理
+  function currying(fn) {
+    let args = []
+
+    function next() {
+      args = args.concat([...arguments])
+      return next
+    }
+    next.toString = function () {
+      return fn.apply(null, args)
+    }
+    next.valueof = function () {
+      return fn.apply(null, args)
+    }
+    return next
   }
 })()
--- readMe.md
+++ readMe.md
@@ -4,4 +4,19 @@
 
 ### getUrlParams 获取地址栏的参数
 
- 直接调用方法获取参数对象，比如  1.html?id=1&name=2 => {id:1,name:2}
+直接调用方法获取参数对象，比如 1.html?id=1&name=2 => {id:1,name:2}
+
+### currying 柯里化
+
+参数：函数 里面对数字进行处理如下：
+
+```javascript
+add = currying(function() {
+  let num = 0
+  num = [...arguments].reduce((a, b) => a + b)
+  return num
+})
+console.log(add(1)(2, 4, 5)(6))
+console.log(add(1,2,4,5,6)
+// 这两个是一样的结果
+```
